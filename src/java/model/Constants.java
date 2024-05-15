8474da06357a120d4138383c9041acb01c50b55a
swnnmt <mactuanson1710@gmail.com>
2024-05-15 09:03:27 +0700

    fist commit

---
+++ src/java/model/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author mactu
+ */
+public class Constants {
+    public static String GOOGLE_CLIENT_ID = "332983765714-q34k91ri5rg8boflvgfl5obstgqsh1sc.apps.googleusercontent.com";
+
+	public static String GOOGLE_CLIENT_SECRET = "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";
+
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/Project_SWP391/loginGoogleHandler";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
